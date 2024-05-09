aff69eb1755ed9698eda55dfbed3bfe59713f847
luisdav1 <luisdav1@users.noreply.github.com>
2024-05-08 20:18:37 -0500

    Mostrar Productos BD

--- Front/js/productos.js
+++ Front/js/productos.js
@@ -1,3 +1,30 @@
+import axios from 'https://cdn.skypack.dev/axios'
+
+async function main() {
+    try {
+        const response = await axios.get('http://localhost:3100/products')
+        mostrarProductos(response.data)
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+main()
+
+function mostrarProductos(productos) {
+    const listProducts = document.querySelector('.contenedor-items')
+    listProducts.innerHTML = ''
+    productos.forEach((producto) => {
+        listProducts.innerHTML += `
+            <div class="item"> 
+                <img src="${producto.img}" alt="" class="img-item">
+                <span class="titulo-item">${producto.name}</span>
+                <span class="precio-item">$${producto.price}</span>   
+            </div>
+        `
+    });
+}
+
 const toggleBtn = document.querySelector('.toggle-btn');
 const toggleBtnIcon = document.querySelector('.toggle-btn i');
 const dropDownMenu = document.querySelector('.dropdown-menu');
